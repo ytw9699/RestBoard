72b2b421ab2292bf294a64ad9b27f579fa10f7c4
champ_home <ytw9699@naver.com>
2022-07-15 02:12:58 +0900

    setting: 프로젝트 세팅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RestBoard
+
 RestBoard
---
+++ RestBoard/src/main/java/com/my/restboard/RestboardApplication.java
@@ -0,0 +1,13 @@
+package com.my.restboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestboardApplication.class, args);
+    }
+
+}
