a720ea41a435ce38a8327f954a5d1563f4947ad5
champ_home <ytw9699@naver.com>
2022-07-15 22:28:44 +0900

    feat: 게시글 조회 api

--- RestBoard/src/main/java/com/my/restboard/application/Board/BoardService.java
+++ RestBoard/src/main/java/com/my/restboard/application/Board/BoardService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -13,13 +14,27 @@ public class BoardService {
 	private final BoardRepository repository;
 
 	@Transactional
-	public Long save(BoardSaveRequestDTO request) {
+	public Long create(BoardSaveRequestDTO request) {
 
 		BoardEntity entity = request.toEntity();
 
 		validate(entity);
 
 		return repository.save(entity).getBoard_num();
+	}
+
+	@Transactional(readOnly = true)
+	public BoardResponseDTO read(Long board_num) {
+
+		Optional<BoardEntity> result = repository.findById(board_num);
+
+		if (!result.isPresent()) {
+			new IllegalArgumentException("해당 하는 글이 없습니다");
+		}
+
+		BoardEntity entity = result.get();
+
+		return new BoardResponseDTO(entity);
 	}
 
 	private void validate(final BoardEntity entity){
