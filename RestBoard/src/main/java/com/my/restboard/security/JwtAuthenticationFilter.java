307c639578e13d0a63685b1ee3266ab5593c6b80
champ_home <ytw9699@naver.com>
2022-07-15 15:32:43 +0900

    feat ,setting: 시큐리티 설정 및 jwt 인증 필터 등록

---
+++ RestBoard/src/main/java/com/my/restboard/security/JwtAuthenticationFilter.java
@@ -0,0 +1,87 @@
+package com.my.restboard.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.restboard.common.CommonResponse;
+import com.my.restboard.common.Error;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private TokenProvider tokenProvider;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+		try {
+
+			String token = parseBearerToken(request);
+
+			if (token != null && !token.equalsIgnoreCase("null")) {
+
+				String userId = tokenProvider.validateAndGetUserId(token);
+
+				AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+								userId,
+								null,
+								AuthorityUtils.NO_AUTHORITIES
+				);
+
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+				securityContext.setAuthentication(authentication);
+				SecurityContextHolder.setContext(securityContext);//SecurityContext에 등록
+			}
+
+			filterChain.doFilter(request, response);
+
+		} catch (Exception ex) {
+
+			log.warn(ex.getMessage());
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			response.setContentType("application/json; charset=UTF-8");
+			response.setStatus(500);
+
+			Error error = Error.builder()
+					.message("인증 되지 않았습니다.")
+					.status(500)
+					.build();
+
+			CommonResponse errorResponse =  CommonResponse.builder()
+					.success(false)
+					.error(error)
+					.build();
+
+			response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+		}
+	}
+
+	private String parseBearerToken(HttpServletRequest request) {
+
+		String bearerToken = request.getHeader("Authorization");
+
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+
+		return null;
+	}
+}
